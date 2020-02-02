662512c198388337d95c4cd31886f427ec1985d8
summer.xia <summer_west2010@126.com>
2020-02-02 23:03:45 +0800

    add Hello World

--- src/com/learning/kotlin/base/Base.kt
+++ src/com/learning/kotlin/base/Base.kt
@@ -1,9 +1,11 @@
 package com.learning.kotlin.base
 
-fun sum(a: Int, b: Int) {
+fun sum(a: Int, b: Int): Int {
     return a + b
 }
 
 fun main(args: Array<String>) {
     println("Hello Kotlin")
+
+    println(sum(1,2))
 }
